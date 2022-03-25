c701436ccff5ae7028cb639bb0d768b5dd4410ec
Majharul tanvir <mdtanvir6070@gmail.com>
2022-03-25 20:20:36 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Chose Your Favourite Chips</h1>
+        </div>
+    );
+};
+
+export default Header;
